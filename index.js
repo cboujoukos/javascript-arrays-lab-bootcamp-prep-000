f5f81d3553fbd72d911849132c550fe10b662b73
Christopher Michael Boujoukos <cboujoukos@gmail.com>
2017-10-20 16:32:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,5 +23,8 @@ function prependKitten(name) {
   return [name, ...kittens]
 }
 function removeLastKitten() {
-  return kittens.slice(-1)
+  return kittens.slice(0, -1)
+}
+function removeFirstKitten() {
+  return kittens.slice(1)
 }
